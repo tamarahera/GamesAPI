0300d155c57cd45a497edcb5750a3de82d69c3b4
tamara_hera <tamara.herashchenko@gmail.com>
2023-09-01 12:20:15 +0200

    update styles

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,9 +1,38 @@
+import Header from '../header/Header';
+import Footer from '../footer/Footer';
+import GameList from '../gameList/GameList';
+import GameRandom from '../gameRandom/GameRandom';
+import GameInfo from '../gameInfo/GameInfo';
+import RawgService from '../services/RawgService';
 import './app.scss';
 
-function App() {
+const App = () => {
+
+
+/*   const newData = new RawgService();
+
+
+  newData.getAllGames()
+    .then((res) => {
+      console.log(res)
+    }) */
+
   return (
     <div className="app">
+      <Header />
+      <main>
 
+        <GameRandom />
+        <section className="games">
+          <div className="container">
+            <div className="games__wrapper">
+              <GameList />
+              <GameInfo />
+            </div>
+          </div>
+        </section>
+      </main>
+      <Footer />
     </div>
   );
 }
