3c619fe108ac04682ae4ae4906b4a939418ca588
tamara_hera <tamara.herashchenko@gmail.com>
2023-09-08 11:47:11 +0200

    update GameInfo and set info about a current game

--- src/components/gameInfo/GameInfo.js
+++ src/components/gameInfo/GameInfo.js
@@ -1,38 +1,73 @@
+import RawgService from '../services/RawgService';
+import { useEffect, useState } from 'react';
+
 import './gameInfo.scss';
 
-const GameInfo = () => {
+
+const GameInfo = ({ currentId }) => {
+    const [game, setGame] = useState(null);
+    
+    useEffect(() => {
+        console.log('use effect')
+        onUpdateGame(currentId);
+    }, [currentId])
+
+    const onUpdateGame = (currentId) => {
+        if (!currentId) {
+            return;
+        }
+
+        const gameData = new RawgService();
+
+        gameData.getGameById(currentId)
+            .then(data => setGame(data))
+            .catch(err => {
+                console.log(err);
+            })
+    }
     return (
         <div className="games__info">
+            <View data={game}/>
+        </div>
+    )
+}
+
+const View = ({ data }) => {
+    const { name, description, developer, img, genres, rating, released, platforms, news, homepage} = data;
+
+    const platformItems = platforms.map((item, i) => {
+        return (
+            <li className="games__info-platform" key={i}>{item}</li>
+        )
+    });
+
+    return (
+        <>
             <div className="games__info-header">
                 <div className="games__info-box">
-                    <img src="https://images.cgames.de/images/gsgp/290/skyrim_6134378.jpg" alt="skyrim"
-                        className="games__info-img" />
+                    <img src={img} alt={name} className="games__info-img" />
                 </div>
-                <h3 className="title">The Elder Scrolls V: Skyrim Special Edition</h3>
+                <h3 className="title">{name}</h3>
                 <div className="games__info-btns">
-                    <a href="#" className="button">HOMEPAGE</a>
-                    <a href="#" className="button button--grey">NEWS</a>
+                    <a href={homepage} className="button" target="_blank" disabled={homepage ? false : true}>HOMEPAGE</a>
+                    <a href={news} className="button button--grey" target="_blank" disabled={news ? false : true}>NEWS</a>
                 </div>
             </div>
             <dl className="games__info-list text">
                 <dt>Released:</dt>
-                <dd>2011</dd>
+                <dd>{released}</dd>
                 <dt>Rating:</dt>
-                <dd>4.5/5</dd>
+                <dd>{rating}/5</dd>
                 <dt>Genres:</dt>
-                <dd>Actions, RPG</dd>
+                <dd>{genres}</dd>
                 <dt>Developer:</dt>
-                <dd>Bethesda Game Studios</dd>
+                <dd>{developer}</dd>
             </dl>
-            <p className="games__info-descr text">Developed by Bethesda Game Studios, the 2011 Studio of the Year, that
-                brought
-                you Oblivion and Fallout 3. Skyrim reimagines and revolutionizes the open-world fantasy epic, bringing
-                to life a complete virtual world open for you to explore any way you choose.</p>
+            <div className="games__info-descr text">{description}</div>
             <ul className="games__info-platforms text">
-                <li className="games__info-platform">PC</li>
-                <li className="games__info-platform">PlayStation 3</li>
+                {platformItems}
             </ul>
-        </div>
+        </>
     )
 }
 
