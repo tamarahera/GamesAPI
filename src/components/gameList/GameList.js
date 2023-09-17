5821a630abfcea7495be2844716b086fb28d727b
tamara_hera <tamara.herashchenko@gmail.com>
2023-09-17 14:42:53 +0200

    load new games by button

--- src/components/gameList/GameList.js
+++ src/components/gameList/GameList.js
@@ -4,37 +4,45 @@ import { useEffect, useState } from 'react';
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 
-const GameList = ({updateCurrentId}) => {
+const GameList = ({ updateCurrentId }) => {
     const [games, setGames] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [newGameLoading, setNewGameLoading] = useState(false); // loading для завантаж додаткових персонажів, вик щоб вимкнути кнопку
     const [error, setError] = useState(false);
+    const [nextUrl, setNextUrl] = useState(null); // лінк на наступні 9 персонажі, який є у базі api
+    const [gamesEnded, setGamesEnded] = useState(false);
 
     useEffect(() => {
-        onLoading();
-
-        onUpdateGames();
+        onRequest(); // перший раз завантажуємо без аргументу, підставляється знач по дефолту - перші 9 ігор
     }, [])
 
-    const onUpdateGames = () => {
+    const onRequest = (nextUrl) => { // другий раз і надалі завантаж з Url зі стейту
+        setNewGameLoading(true); // load new games, в цей час робимо кнопку неактивною
+
         const gamesData = new RawgService();
 
-        gamesData.getAllGames()
-            .then(data => {setGames(data)})
-            .then(() => setLoading(false))
+        gamesData.getAllGames(nextUrl)
+            .then(data => {
+                onNewGamesLoaded(data.arr, data.nextPageUrl);
+            })
             .catch(err => {
                 console.log(err);
                 onError();
             })
     }
 
+    const onNewGamesLoaded = (newGamesList, nextPageUrl) => {
+        setGames(games => [...games, ...newGamesList]); // старі ігри + нові
+
+        nextPageUrl ? setNextUrl(nextPageUrl) : setGamesEnded(true); // якщо url неправда, персонажі що закінчилися ставимо в true і вик це значення щоб сховати кнопку
+
+        setLoading(false);
+        setNewGameLoading(false);
+    }
+
     const onError = () => {
         setLoading(false);
         setError(true);
-    }
-
-    const onLoading = () => {
-        setLoading(true);
-        setError(false);
     }
 
     const createList = (arr) => {
@@ -61,12 +69,12 @@ const GameList = ({updateCurrentId}) => {
 
     const spinner = loading ? <Spinner /> : null;
     const errorMessage = error ? <ErrorMessage /> : null;
-    const content = spinner || errorMessage || list
+    const content = spinner || errorMessage || list;
 
     return (
         <div className="games__content">
             {content}
-            <button className="button games__content-btn" type="button">LOAD MORE</button>
+            <button className="button games__content-btn" type="button" onClick={() => onRequest(nextUrl)} disabled={newGameLoading} style={{'display': gamesEnded ? 'none' : 'block'}}>LOAD MORE</button>
         </div>
     )
 }
