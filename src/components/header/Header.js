70c374539f2104aa2a24769986febbf80700a04c
tamara_hera <tamara.herashchenko@gmail.com>
2023-11-12 10:50:33 +0100

    add links with an active class for navigation

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,16 +1,17 @@
+import { Link, NavLink } from 'react-router-dom';
 import './header.scss';
 
 const Header = () => {
     return (
         <header className="header">
-            <a href="#" className="logo">
+            <Link to="/" className="logo">
                 <h1><span>Games</span> information portal</h1>
-            </a>
+            </Link>
             <hr />
             <nav className="header__nav">
-                <a href="#" className="header__nav-link header__nav-link--active">Games</a>
-                {' / '} 
-                <a href="#" className="header__nav-link">More about</a>
+                <NavLink exact to="/" className="header__nav-link" activeClassName="header__nav-link--active">Games</NavLink>
+                {' / '}
+                <NavLink exact to="/genres" className="header__nav-link" activeClassName="header__nav-link--active">More about</NavLink>
             </nav>
         </header>
     )
