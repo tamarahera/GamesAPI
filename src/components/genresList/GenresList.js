8451b630a1805149e142e1ecc6c00b8ae402fa81
tamara_hera <tamara.herashchenko@gmail.com>
2023-11-20 16:07:29 +0100

    add Spinner, ErrorMessage, request genres data by button click, change buttons content

--- src/components/genresList/GenresList.js
+++ src/components/genresList/GenresList.js
@@ -2,26 +2,34 @@ import { useState, useEffect } from "react";
 import './genresList.scss';
 import useRawgService from "../services/RawgService";
 import controller from '../../resources/controller_white.jpg';
+import imageNotFound from '../../resources/image_not_found.jpg';
+import Spinner from "../spinner/Spinner";
+import ErrorMessage from "../errorMessage/ErrorMessage";
 
 
 const GenresList = () => {
     const [genresData, setGenresData] = useState([]);
-    const { getGenres } = useRawgService();
+    const [genresDataLoading, setGenresDataLoading] = useState(false);
+    const [openGames, setOpenGames] = useState(false);
 
-    useEffect(() => {
-        onRequestGenres();
-    }, []);
+    const { getGenres, getGameById, loading, error } = useRawgService();
 
     const onRequestGenres = () => {
+        setOpenGames(false);
+        setGenresDataLoading(true);
+        if (genresData.length > 0) {
+            return;
+        } else {
         getGenres()
             .then(data => {
                 console.log(data);
-                genresLoaded(data);
+                    setGenresData(data);
             })
+                .catch(() => {
+                    setGenresDataLoading(false);
+                })
+        }
     }
-
-    const genresLoaded = (newData) => {
-        setGenresData(newData);
     }
 
     const createGenresList = (arr) => {
@@ -37,10 +45,20 @@ const GenresList = () => {
                 </li>
             )
         });
+        return (
+            <>
+                <ul className="genres__list">
+                    {items}
+                </ul>
+            </>
+        )
+    }
     }
 
-    const genresList = createGenresList(genresData);
-    const content = genresList ? genresList : null;
+    const genresList = genresData.length && !openGames > 0 ? createGenresList(genresData) : null;
+    const spinner = loading ? <Spinner /> : null;
+    const errorMessage = error ? <ErrorMessage /> : null;
+    const content = spinner || errorMessage || genresList;
     return (
         <>
             <div className="genres__promo">
@@ -53,11 +71,15 @@ const GenresList = () => {
                         {`Choose `} <span>a genre</span> {' and explore the most popular games'}
                     </p>
                 </div>
-                <button className="button genres__promo-btn" type="button">{'Let`s start'}</button>
+                <button
+                    className="button genres__promo-btn"
+                    type="button"
+                    onClick={onRequestGenres}
+                    disabled={genresDataLoading}>
+                    {openGames ? 'Back to genres' : 'Let`s start'}
+                </button>
             </div>
-            <ul className="genres__list">
-                {genresList}
-            </ul>
+            {content}
         </>
     )
 }
