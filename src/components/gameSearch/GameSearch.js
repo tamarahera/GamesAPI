dd59655852d9324c5f825ea9cf1490a3a9b6076d
tamara_hera <tamara.herashchenko@gmail.com>
2023-12-12 15:39:31 +0100

    update search component styles

--- src/components/gameSearch/GameSearch.js
+++ src/components/gameSearch/GameSearch.js
@@ -7,7 +7,38 @@ import useRawgService from '../services/RawgService';
 import './gameSearch.scss';
 
 const GameSearch = () => {
-    const [foundGames, setFoundGames] = useState(null);
+    const [foundGames, setFoundGames] = useState([
+        {
+            "id": 14022,
+            "name": "DUNGEONS",
+            "background_image": "https://media.rawg.io/media/screenshots/f7b/f7b1cf81632880407321f8e3385c5ee6.jpg",
+            "released": "2011-02-04"
+        },
+        {
+            "id": 7485,
+            "name": "Elite Dangerous",
+            "background_image": "https://media.rawg.io/media/games/b69/b69a67833630dd96d8eee9d2c8c27574.jpg",
+            "released": "2015-04-02"
+        },
+        {
+            "id": 18454,
+            "name": "Crossfire: Dungeons",
+            "background_image": "https://media.rawg.io/media/screenshots/d64/d6400b91cc1ee03708032a4e0fa73ca2.jpg",
+            "released": "2015-05-21"
+        },
+        {
+            "id": 257195,
+            "name": "Minecraft: Dungeons",
+            "background_image": "https://media.rawg.io/media/games/c14/c146d28ceb14c84ea9fdbd7410701277.jpg",
+            "released": "2020-05-26"
+        },
+        {
+            "id": 20990,
+            "name": "Dungeon-Party",
+            "background_image": "https://media.rawg.io/media/screenshots/c44/c44b5bc1923d26c200c311fd997f4e33.jpg",
+            "released": "2013-03-13"
+        }
+    ]);
     const { getGamesBySearch } = useRawgService();
 
     const onRequest = (value) => {
@@ -33,8 +64,8 @@ const GameSearch = () => {
                     <div className="search__item-box">
                         <img src={background_image} alt={name} className="search__item-img" />
                     </div>
-                    <h4 className="search__item-title ">{name}</h4>
-                    <p className="search__released text">
+                    <h4 className="search__item-title">{name}</h4>
+                    <p className="search__released">
                         <i>Released:</i> <br />
                         {released}
                     </p>
@@ -71,8 +102,8 @@ const GameSearch = () => {
                                 <Form className="search__form">
                                     <label
                                         htmlFor="name"
-                                        className='title'>
-                                        Or find a game by name:
+                                        className='search__label'>
+                                        Find a game by name:
                                     </label>
                                     <div className="search__box">
                                         <Field
@@ -98,7 +129,7 @@ const GameSearch = () => {
                                         onResetSearch();
                                         props.handleReset();
                                     }}>
-                                    <i className='text'>Reset all</i>
+                                    <i>Reset all</i>
                                 </button> : null}
                             </>
 
