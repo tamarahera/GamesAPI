1e5f495a9336611b81e6faa54b5848e62cb004ab
tamara_hera <tamara.herashchenko@gmail.com>
2024-01-23 14:13:16 +0100

    add event to change the theme

--- src/components/colorModeSwitcher/ColorModeSwitcher.js
+++ src/components/colorModeSwitcher/ColorModeSwitcher.js
@@ -1,12 +1,30 @@
-import React from 'react'
-import './colorModeSwitcher.scss';
 import Moon from '../../resources/moon.svg';
 import Sun from '../../resources/sun.svg';
 
+import { useEffect, useState } from 'react';
+
+import './colorModeSwitcher.scss';
+
 const ColorModeSwitcher = () => {
+    const [theme, setTheme] = useState('dark');
+
+    useEffect(() => {
+        document.body.setAttribute('data-theme', theme);
+    }, [theme]);
+
+    const onChangeTheme = (e) => {
+        if (e.target.checked === true) {
+            setTheme('dark');
+        } else {
+            setTheme('light');
+        }
+    }
+
     return (
         <div className="switcher">
-            <input className="switcher__input" type="checkbox" id="darkmode-toggle" />
+            <input className="switcher__input" type="checkbox" id="darkmode-toggle"
+                onChange={onChangeTheme}
+                checked={theme === 'dark'} />
             <label className="switcher__label" htmlFor="darkmode-toggle">
                 <img className="switcher__img switcher__img-moon" src={Moon} alt="moon" />
                 <img className="switcher__img switcher__img-sun" src={Sun} alt="moon" />
