7303a38382336efc1d302869d3f04df2bb9f0d2e
tamara_hera <tamara.herashchenko@gmail.com>
2024-01-30 15:13:35 +0100

    change dark mode by Tab, turn off user-select

--- src/components/colorModeSwitcher/ColorModeSwitcher.js
+++ src/components/colorModeSwitcher/ColorModeSwitcher.js
@@ -7,13 +7,15 @@ import './colorModeSwitcher.scss';
 
 const ColorModeSwitcher = () => {
     const [theme, setTheme] = useState('dark');
+    const [checkInput, setCheckInput] = useState(false);
 
     useEffect(() => {
         document.body.setAttribute('data-theme', theme);
     }, [theme]);
 
-    const onChangeTheme = (e) => {
-        if (e.target.checked === true) {
+    const onChangeTheme = (checkInput) => {
+        setCheckInput(checkInput => !checkInput);
+        if (checkInput) {
             setTheme('dark');
         } else {
             setTheme('light');
@@ -22,10 +24,19 @@ const ColorModeSwitcher = () => {
 
     return (
         <div className="switcher">
-            <input className="switcher__input" type="checkbox" id="darkmode-toggle"
-                onChange={onChangeTheme}
+            <input className="switcher__input"
+                type="checkbox"
+                id="darkmode-toggle"
+                onChange={() => onChangeTheme(checkInput)}
                 checked={theme === 'dark'} />
-            <label className="switcher__label" htmlFor="darkmode-toggle">
+            <label className="switcher__label"
+                htmlFor="darkmode-toggle"
+                tabIndex="0"
+                onKeyUp={(e) => {
+                    if (e.key === 'Enter') {
+                        onChangeTheme(checkInput);
+                    }
+                }}>
                 <img className="switcher__img switcher__img-moon" src={Moon} alt="moon" />
                 <img className="switcher__img switcher__img-sun" src={Sun} alt="moon" />
 
