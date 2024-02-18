4e16b8e5da80aad4bf072f55d076676d110532ad
tamara_hera <tamara.herashchenko@gmail.com>
2024-02-18 20:02:13 +0100

    change modal close button

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -1,14 +1,14 @@
-import React, { useState } from 'react'
 import './modal.scss';
-import Close from '../../resources/close.svg'
 
 const Modal = ({ modal, showModal, src }) => {
+
     return (
         <div className="modal">
             <div className="modal__overlay" onClick={() => showModal(modal)}></div>
             <div className="modal__content">
                 <button className="modal__close" type="button" onClick={() => showModal(modal)}>
-                    <img src={Close} alt="close" className="modal__close-img" />
+                    <span />
+                    <span />
                 </button>
                 <div className="modal__box">
                     <img src={src} alt="game" className="modal__box-img" />
